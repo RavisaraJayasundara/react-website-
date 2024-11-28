f4397a7fa59fc51bfdae250d277aebaf3ae4255a
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 00:45:10 +0530

    update component file (food file)

--- react_website/src/components/Food.js
+++ react_website/src/components/Food.js
@@ -1,9 +1,9 @@
-import React,{useState} from 'react';
-
-
+import {FoodInfo} from './foodData';
+import {useState} from 'react';
 export default function Food(){
-    var food=["Cake","Rice","Bun","Biscuts"];
+    var foods=["Cake","Pizza","Burger","Muffin","Noodles","Rice","Chocolate"];
     const[index,setIndex]=useState(0);
+
     const prev=()=>{
         if(index<1){
             alert('Out of range');
@@ -12,17 +12,22 @@ export default function Food(){
         }
     }
     const next=()=>{
-        if (index>food.length-2) {
+        if(index>FoodInfo.length-2){
             alert('Out of range');
-        } else {
+        }else{
             setIndex(index+1);
         }
     }
+
     return(
         <div>
-            <h1>{food[index]}</h1>
-            <button onClick={prev}>Previous</button>
-            <button onClick={next}>Next</button>
+             <h1>Foods</h1>
+             <h2>{foods[index]}</h2>
+             <img src={FoodInfo[index].image}></img>
+             <p>{FoodInfo[index].description}</p>
+             <button onClick={prev}>Previous</button>
+             <button onClick={next}>Next</button>
+
         </div>
     )
 }
---
+++ react_website/src/components/foodData.js
@@ -0,0 +1,25 @@
+export const FoodInfo=[{
+    name:'Cake',
+    image:'https://i.pinimg.com/736x/96/a9/8a/96a98a95980bcb4c052ee9ec82f4a7fc.jpg',
+    description:'A cake is a sweet baked good that is usually made from flour, sugar, eggs, and other ingredients'
+},
+{
+    name:'Pizza',
+    image:'https://suebeehomemaker.com/wp-content/uploads/2024/09/supreme-pizza-recipe-6.jpg',
+    description:' a dish made typically of thinly rolled bread dough spread with a spiced mixture usually including tomatoes and cheese and often other toppings and baked.'
+},
+{
+    name:'Burger',
+    image:'https://www.clubhouse.ca/-/media/project/oneweb/clubhouseca/recipes/ultimate_canadian_burgers-mobile.jpg?rev=ebeb4286a607432292afc59794baf0fc&vd=20240626T041239Z&extension=webp&hash=AC36848282EBD7BAFEEBB091250C3175',
+    description:'A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor. Burgers are considered an American food but are popular around the world.'
+},
+{
+    name:'Muffin',
+    image:'https://www.olgainthekitchen.com/wp-content/uploads/2020/03/Sour-Cream-Blueberry-Muffins-Recipe-4.jpg',
+    description:'Muffins are single-serving quick breads, which rise with the help of baking soda or baking powder and eggs instead of yeast'
+},
+{
+    name:'Noodles',
+    image:'https://hot-thai-kitchen.com/wp-content/uploads/2021/11/chili-garlic-noodles-blog.jpg',
+    description:'Noodles are a type of food made from unleavened dough that is rolled out, cut, stretched, or extruded into long strips or strings. '
+}]
