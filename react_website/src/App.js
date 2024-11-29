8223c2b612edb53b0bc96defb2e52da0df2f83b6
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 10:26:26 +0530

    add light theme , dark theme

--- react_website/src/App.js
+++ react_website/src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Arts from "./components/Arts";
 import Fruits from "./components/Fruits";
 import Food from "./components/Food";
+import {useState} from 'react';
 
 function App() {
+  const[theme,setTheme]=useState(true);
+  const lightTheme={
+       backgroundColor :'white',
+       color :'black'
+  }
+  const darkTheme={
+    backgroundColor:'black',
+    color:'white'
+  }
   return (
     <>
-       <Arts></Arts>
-       <Food></Food>
-       <Fruits></Fruits>
+      <div style={theme?lightTheme:darkTheme}>
+        <button onClick={()=>setTheme(!theme)}>{theme?'Light Theme':'Dark Theme'}</button>
+        <Arts></Arts>
+        <Food></Food>
+        <Fruits></Fruits>
+      </div>
     </>
   );
 }
